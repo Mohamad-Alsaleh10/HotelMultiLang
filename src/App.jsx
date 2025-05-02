3c91bff676a3b9f7e6cb1d02e1608cbbe5379623
Mohamad Alsaleh <eng.mohamad110@gmail.com>
2025-05-02 23:40:16 +0300

    'h'

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   };
   return (
     <div>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="HotelMultiLang">
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/room/:id" element={<RoomDetails />} />
